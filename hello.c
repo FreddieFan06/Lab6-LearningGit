79e63ab2455f79d2b511b4c2afbb07fe1c0b047d
Joshua Puim <jpuim@uoguelph.ca>
2025-10-30 20:24:11 -0400

    Added functionality to be able to hit the twin towers again in 2025

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+
+void guideMissleTT()
+{
+    printf("THEY HIT THE SECOND TOWER!\n");
+}
+
+
 int main(void) {
     // Here is a comment to make a change heheheheheh
     printf("Hello World!\n");
+
+    guideMissleTT();
+
     return 0;
 }
