4bfee9a030162ca9c21f32fadb9d41ca6c12242a
Joshua Puim <jpuim@uoguelph.ca>
2025-10-30 20:27:00 -0400

    added new function to print a funny :)

--- hello.c
+++ hello.c
@@ -6,12 +6,18 @@ void guideMissleTT()
     printf("THEY HIT THE SECOND TOWER!\n");
 }
 
+void sixSeven()
+{
+    printf("67676767767676767\n");
+}
+
 
 int main(void) {
     // Here is a comment to make a change heheheheheh
     printf("Hello World!\n");
 
     guideMissleTT();
+    sixSeven();
 
     return 0;
 }
